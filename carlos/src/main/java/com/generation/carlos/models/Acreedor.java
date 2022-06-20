7492efdf945f1af0435d4b5ac210e86d26ab4218
carlos iturra gonzález <carlitos0@live.cl>
2022-06-20 15:02:21 -0400

    modelo

--- carlos/src/main/java/com/generation/carlos/models/Acreedor.java
+++ carlos/src/main/java/com/generation/carlos/models/Acreedor.java
@@ -1,5 +1,48 @@
 package com.generation.carlos.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Acreedores")
 public class Acreedor {
+    
+    //atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+
+    //constructores
+    public Acreedor() {
+    }
+    public Acreedor(Long id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    //getters y setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    } 
     
 }
